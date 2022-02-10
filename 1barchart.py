4a3f66a2dbad76fd6c7a1af2560a63bd8bd20a27
sharonk1999 <78010314+sharonk1999@users.noreply.github.com>
2022-02-10 09:29:22 +0530

    Delete 1barchart.py

--- 1barchart.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as pll
-objects=('java','python','javascript','c#','c++')
-p=(22.2,8.8,8,77,6.7)
-pll.bar(objects,p,align='center',alpha=0.4,color='blue')
-p11.label('popularity')
-p11.label('programming language')
-p11.title('programming language popularity')
-pll.show()
