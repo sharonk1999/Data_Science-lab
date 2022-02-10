5da7df377688ce179c9dfb2a289331cbd3fbdf90
sharonk1999 <78010314+sharonk1999@users.noreply.github.com>
2022-02-10 09:17:59 +0530

    Add files via upload

---
+++ 1barchart.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+objects=('java','python','javascript','c#','c++')
+p=(22.2,8.8,8,77,6.7)
+pll..bar(objects,p,align='center',alpha=0.4,color='blue')
+p11.label('popularity')
+p11.label('programming language')
+p11.title('programming language popularity')
+pll.show()
